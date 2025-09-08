7d2d1d0fbe334d520be4c45e68ae4f2bd5a3ec04
PhmHung <hungk77777@gmail.com>
2025-09-08 21:35:57 +0700

    Basic syntax Python review

---
+++ D1_Basic/do_for.py
@@ -0,0 +1,12 @@
+
+arr1 = [1, 2, 3, 4, 5]
+for i in arr1:
+    if i%2 ==0 :
+        print(arr1.index(i), i)
+
+print ('--- IGNORE ---')
+
+arr2 = [6,7,8,9,10]
+for i in arr2:
+    if i % 2 == 0:
+        print(arr2.index(i), i)
